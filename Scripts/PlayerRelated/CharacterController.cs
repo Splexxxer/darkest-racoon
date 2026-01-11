464acc8f2ec71fe335704b36460c670070d07883
mazzel <marcel.schlipf@gmx.de>
2026-01-11 17:06:24 +0100

    Player is not a rect anymore and has a hurtbox now

---
+++ Scripts/PlayerRelated/CharacterController.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+public partial class CharacterController : CharacterBody2D
+{
+	[Export] public float Speed = 600f;
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 dir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		if (dir.LengthSquared() > 1f)
+			dir = dir.Normalized();
+
+		Velocity = dir * Speed;
+		MoveAndSlide();
+	}
+}
